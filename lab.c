374d7148b3836a032e0988298d94d77e7f8cc19d
eugenleshchov <eugenleshchov@gmail.com>
2017-04-09 13:57:08 +0300

    simplify taylor series members calculation, reduce open processes number to number of members

--- lab.c
+++ lab.c
@@ -54,44 +54,73 @@ int main(int argc[], char *argv[])
   int i;
   int j;
 
+	int processCounter = 0;
+
   // process Tailor's decomposition
   for (i = 0; i < N; i++) {
-    for (j = 0; j < n; j++) {
+    for (j = 1; j <= n; j++) {
+			if (processCounter >= n) {
+				int status;
+				pid_t done = wait(&status);
+				processCounter--;
+			}
+
+			processCounter++;
       pid_t pid = fork();
-
       if (pid == 0) {
-        struct TailorProcessInfo psInfo;
-
-        psInfo.pid = getpid();
+      	struct TailorProcessInfo psInfo;
+	      psInfo.pid = getpid();
         psInfo.elementIndex = i;
+	      // long double index = powl(-1, j - 1);
+			  long double index = (j - 1) % 2 == 0 ? 1 : -1;
+	      long double fact = factorial(2 * j - 1);
+				long double angle = 2 * PI * i / N;
 
-        // long double index = powl(-1, j - 1);
-				long double index = (j - 1) % 2 == 0 ? 1 : -1;
-        long double fact = factorial(2 * j - 1);
-        long double x = powl(2 * PI * i / N, 2 * j - 1);
+				if (angle > (PI / 2) && angle < PI) {
+					angle = (PI / 2) - (angle - (PI / 2));
+				}
+
+				if (angle > PI) {
+					angle = (PI - (angle - PI));
+					angle = -((PI / 2) - (angle - (PI / 2)));
+				}
+
+        long double x = powl(angle, 2 * j - 1);
         psInfo.elementValue = index / fact * x;
 
-        // printf("\npid: %d | i: %d | j: %d | element value: %Le | index: %Le | fact: %Le | x: %Le\n",
-        //   psInfo.pid,
-        //   psInfo.elementIndex,
-        //   j,
-        //   psInfo.elementValue,
-        //   index,
-        //   fact,
-        //   x
-        // );
+	      // printf("\npid: %d | i: %d | j: %d | element value: %Le | index: %Le | fact: %Le | x: %Le\n",
+	      //   psInfo.pid,
+	      //   psInfo.elementIndex,
+	      //   j,
+	      //   psInfo.elementValue,
+	      //   index,
+	      //   fact,
+	      //   x
+	      // );
+	      printf("\npid: %d | y[%d][%d] | element value: %Le\n",
+	        psInfo.pid,
+	        i,
+					j,
+	        psInfo.elementValue
+	      );
 
-        if (write(processesStoreFile, &psInfo, sizeof(struct TailorProcessInfo)) < 0) {
+	      if (write(processesStoreFile, &psInfo, sizeof(struct TailorProcessInfo)) < 0) {
 					perror(basename(execName));
 					exit(1);
 				};
 
-        exit(0);
-      } else if (pid > 0) {
-        continue;
-      } else {
-        perror(execName);
-      }
+	      exit(0);
+	    } else if (pid > 0) {
+	      continue;
+	    } else {
+	      perror(execName);
+	    }
+			// } else {
+			// 	int status;
+			// 	pid_t done = wait(&status);
+			// 	processCounter--;
+			// 	j--;
+			// }
     }
   }
 
